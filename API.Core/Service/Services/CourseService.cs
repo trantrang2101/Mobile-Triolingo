88fa05c042962fe65a9d7f3cd695052490ee92a4
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-28 22:12:37 +0700

    build base for API, CRUD Course API

---
+++ API.Core/Service/Services/CourseService.cs
@@ -0,0 +1,69 @@
+﻿using API.Core.Common;
+using API.Core.DataAccess;
+using API.Core.Entity;
+using Microsoft.AspNetCore.Http;
+using API.Core.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Core.Service.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly TriolingoDbContext _context;
+        public CourseService(TriolingoDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> CreateCourse(Course course)
+        {
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteCourse(int courseId)
+        {
+            var co = await _context.Courses.SingleOrDefaultAsync(x => x.Id == courseId);
+            if (co != null)
+            {
+                _context.Courses.Remove(co);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public List<Course> GetAllCourses()
+        {
+            return _context.Courses.ToList();
+        }
+
+        public async Task<bool> UpdateCourse(Course course)
+        {
+            var co = await _context.Courses.SingleOrDefaultAsync(x => x.Id == course.Id);
+            if (co != null)
+            {
+                co.Description = course.Description;
+                co.RateAverage = course.RateAverage;
+                co.Note = course.Note;
+                co.Name = course.Name;
+                co.Status = course.Status;
+                _context.Courses.Update(co);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+        #region private method
+        //private string ValidateAddCourse(Course course)
+        //{
+            
+        //} 
+        #endregion
+    }
+}
---
+++ APIService/Controllers/Courses/CourseController.cs
@@ -0,0 +1,75 @@
+﻿using API.Core.Entity;
+using API.Core.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIService.Controllers.Courses
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        [HttpGet]
+        public IActionResult GetAllCourse()
+        {
+            try
+            {
+                return Ok(_courseService.GetAllCourses());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCourse(Course co)
+        {
+            try
+            {
+                if (await _courseService.CreateCourse(co))
+                    return Ok(co);
+                else return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCourse(Course co)
+        {
+            try
+            {
+                if (await _courseService.UpdateCourse(co))
+                    return NoContent();
+                else return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+        }
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteCourse(int coId)
+        {
+            try
+            {
+                if (await _courseService.DeleteCourse(coId)) return Ok();
+                else return BadRequest();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+        }
+    }
+}
