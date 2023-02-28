a51ad4b5c6fbde2f49b8b9e315349472992383f5
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-28 22:22:34 +0700

    add core API

---
+++ API.Core/Service/Interface/ICourseService.cs
@@ -0,0 +1,18 @@
+﻿using API.Core.Common;
+using API.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Core.Service.Interface
+{
+    public interface ICourseService
+    {
+        List<Course> GetAllCourses();
+        Task<bool> CreateCourse(Course course);
+        Task<bool> UpdateCourse(Course course);
+        Task<bool> DeleteCourse(int courseId);
+    }
+}
