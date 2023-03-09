3f6c2ca7cd658fc6301969a4d832cc79748cfa53
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-03-09 17:01:36 +0700

    design unit scene, add model for unit and lesson

--- Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Units/ListUnits.java
+++ Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Units/ListUnits.java
@@ -2,9 +2,14 @@ package com.example.triolingo_mobile.Units;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.example.triolingo_mobile.Lesson.ListenChoice.ListenChoiceActivity;
 import com.example.triolingo_mobile.R;
+import com.example.triolingo_mobile.Util.LessonUtil;
 
 public class ListUnits extends AppCompatActivity {
 
@@ -12,5 +17,17 @@ public class ListUnits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_units);
+        ImageButton bt = findViewById(R.id.bt_Unit);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListUnits.this, LessonProgress.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void MoveToLesson(View view) {
+
     }
 }
