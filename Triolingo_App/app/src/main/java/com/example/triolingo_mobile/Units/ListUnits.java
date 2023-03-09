5d15c382a881e563fbb948cc464c8fac6c946ce9
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-03-09 09:47:22 +0700

    create units scene

---
+++ Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Units/ListUnits.java
@@ -0,0 +1,16 @@
+package com.example.triolingo_mobile.Units;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.triolingo_mobile.R;
+
+public class ListUnits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_units);
+    }
+}
