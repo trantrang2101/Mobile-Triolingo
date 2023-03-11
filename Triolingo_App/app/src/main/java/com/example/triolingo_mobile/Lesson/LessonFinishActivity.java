ca57cf43335fa1a1d1547bd332a51400808c8d5f
Minnminx <theultimatrix2@gmail.com>
2023-03-12 04:45:48 +0700

    added lesson finish screen

---
+++ Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Lesson/LessonFinishActivity.java
@@ -0,0 +1,14 @@
+package com.example.triolingo_mobile.Lesson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LessonFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lession_finish);
+    }
+}
