4999f065e37a3fe2fa8bfbb88f1dfbe66cfd7975
trantrang2101 <hanhuyettu.cool@gmail.com>
2023-03-12 19:04:41 +0700

    update

--- APIService/Program.cs
+++ APIService/Program.cs
@@ -40,7 +40,7 @@ builder.Services.AddTransient<IUnitService, UnitService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()||app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
--- Triolingo_App/app/src/main/java/com/example/triolingo_mobile/API/RetrofitClient.java
+++ Triolingo_App/app/src/main/java/com/example/triolingo_mobile/API/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://koutahara.com/";
+    private static final String BASE_URL = "https://www.koutahara.com/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getRetrofitInstance() {
