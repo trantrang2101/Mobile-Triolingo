a576136051cd630cc5f13b35d104c3fcfdc3608f
Minnminx <theultimatrix2@gmail.com>
2023-03-19 21:01:47 +0700

    Update word matching activity #3

---
+++ Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Lesson/WordMatching/WordListAdapter.java
@@ -0,0 +1,77 @@
+package com.example.triolingo_mobile.Lesson.WordMatching;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.triolingo_mobile.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordItemHolder> {
+    private HashMap<String, Integer> wordMap;
+    private OnWordItemClick _listener;
+    private WordItemHolder oldSelected;
+    private List<WordItemHolder> _cache;
+
+    public WordListAdapter(HashMap<String, Integer> wordMap, OnWordItemClick listener) {
+        this.wordMap = wordMap;
+        this._listener = listener;
+        this._cache = new ArrayList<>();
+        oldSelected = null;
+    }
+
+    @NonNull
+    @Override
+    public WordItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.lesson_word_matching_row, parent, false);
+        WordItemHolder viewHolder = new WordItemHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordItemHolder holder, int position) {
+        String word = new ArrayList<>(wordMap.keySet()).get(position);
+        holder.SetWord(word);
+        holder.setOnClick(v -> {
+            int returnCd = _listener.onItemClickListener(holder);
+            if (returnCd == 0 && oldSelected != null) {
+                // unselect any selected one
+                oldSelected.UnSelect();
+            }
+            else if (returnCd != 0) {
+                oldSelected = null;
+            }
+            else {
+                oldSelected = holder;
+            }
+            holder.setButtonBg(returnCd);
+        });
+        _cache.add(holder);
+    }
+
+    public void FinalizeLastSelected(boolean isCorrect) {
+        if (oldSelected != null) {
+            oldSelected.setButtonBg(isCorrect ? 1 : -1);
+        }
+        oldSelected = null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return wordMap.size();
+    }
+
+    void MarkAllIncorrect() {
+        for (WordItemHolder item : _cache) {
+            if (item.isClickable()) {
+                item.setButtonBg(-1);
+            }
+        }
+    }
+}
--- Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Lesson/WordMatching/WordMatchingActivity.java
+++ Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Lesson/WordMatching/WordMatchingActivity.java
@@ -1,35 +1,152 @@
 package com.example.triolingo_mobile.Lesson.WordMatching;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.example.triolingo_mobile.DAO.ExerciseDAO;
 import com.example.triolingo_mobile.DAO.LessonDAO;
+import com.example.triolingo_mobile.DAO.QuestionDAO;
+import com.example.triolingo_mobile.Model.AnswerModel;
+import com.example.triolingo_mobile.Model.Question;
 import com.example.triolingo_mobile.R;
 import com.example.triolingo_mobile.Util.LessonUtil;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class WordMatchingActivity extends AppCompatActivity {
-    private HashMap<String, String> Question_Answer;
+    private HashMap<String, Integer> QuestionColumn;
+    private HashMap<String, Integer> AnswerColumn;
+    private RecyclerView questionColumn;
+    private RecyclerView answerColumn;
+    private int curPoint;
+    private String selectedQuestion;
+    private String selectedAnswer;
+    private WordListAdapter questionAdapter;
+    private WordListAdapter answerAdapter;
+    private List<Question> questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_matching_execise);
+        questionColumn = findViewById(R.id.question_recyclerview);
+        answerColumn = findViewById(R.id.answer_recyclerview);
+
+        GetQuestionData();
     }
 
     void GetQuestionData() {
-//        var LessonDAO
         Intent intent = getIntent();
-        int exerciseID = intent.getIntExtra("exerciseId", -1);
-        if (exerciseID < 0) {
-            // next question
+        int exerciseID = intent.getIntExtra("exerciseId", 11);
+        questions = QuestionDAO.getInstance().getQuestionsByExId(exerciseID);
+        curPoint = 0;
+        QuestionColumn = new HashMap<>();
+        AnswerColumn = new HashMap<>();
+        for (Question question : questions) {
+            List<AnswerModel> answers = ExerciseDAO.getInstance().getAnswerOfQuestion(question.getId(), "STATUS > 0 AND IsCorrect > 0");
+            if (question.getQuestion1() != null && answers.size() > 0) {
+                QuestionColumn.put(question.getQuestion1(), question.getId());
+                AnswerColumn.put(answers.get(0).getAnswer(), question.getId());
+                curPoint += question.getMark();
+            }
+        }
+
+        questionAdapter = new WordListAdapter(QuestionColumn, this::OnQuestionClick);
+        questionColumn.setAdapter(questionAdapter);
+        questionColumn.setLayoutManager(new LinearLayoutManager(this));
+        answerAdapter = new WordListAdapter(AnswerColumn, this::OnAnswerClick);
+        answerColumn.setAdapter(answerAdapter);
+        answerColumn.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    int OnAnswerClick(WordItemHolder holder) {
+        selectedAnswer = holder.getWord();
+        if (selectedQuestion == null) {
+            // no question is selected yet
+            return 0; // 0 mean selected
+        }
+        else {
+            // selected an answer, then a question
+            boolean isCorrect = QuestionColumn.get(selectedQuestion) == AnswerColumn.get(selectedAnswer);
+            questionAdapter.FinalizeLastSelected(isCorrect);
+            RemoveWord(isCorrect);
+            DeselectAll();
+            return isCorrect ? 1 : -1;
         }
     }
 
-    void finishExecercise() {
+    void RemoveWord(boolean isCorrect) {
+        if (selectedQuestion == null)
+            return;
 
+        int id = QuestionColumn.containsKey(selectedQuestion) ? QuestionColumn.get(selectedQuestion) : -1;
+        if (!isCorrect) {
+            for (Question q : questions) {
+                if (q.getId() == id) {
+                    curPoint -= q.getMark();
+                    break;
+                }
+            }
+        }
+        QuestionColumn.remove(selectedQuestion);
+        AnswerColumn.remove(selectedAnswer);
+        if (IsFinished()) {
+            // next exercise
+            for (int qId : QuestionColumn.values()) {
+                for (Question question : questions) {
+                    if (qId == question.getId()) {
+                        curPoint -= question.getMark();
+                        break;
+                    }
+                }
+            }
+            questionAdapter.MarkAllIncorrect();
+            answerAdapter.MarkAllIncorrect();
+            Log.i("QnA", "FINISHED: " + curPoint);
+        }
+    }
+
+    boolean IsFinished() {
+        if (QuestionColumn.size() == 1) {
+            // only 1 left
+            return true;
+        }
+        for (int question : QuestionColumn.values()) {
+            for (int answer : AnswerColumn.values()) {
+                if (answer == question) {
+                    // there's still answer for the question
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    int OnQuestionClick(WordItemHolder holder) {
+        selectedQuestion = holder.getWord();
+        if (selectedAnswer == null) {
+            // no answer is selected yet
+            return 0; // 0 mean selected
+        }
+        else {
+            // selected a question, then an answer
+            boolean isCorrect = QuestionColumn.get(selectedQuestion) == AnswerColumn.get(selectedAnswer);
+            answerAdapter.FinalizeLastSelected(isCorrect);
+            RemoveWord(isCorrect);
+            DeselectAll();
+            return isCorrect ? 1 : -1;
+        }
+    }
+
+    void DeselectAll() {
+        selectedQuestion = null;
+        selectedAnswer = null;
     }
 }
