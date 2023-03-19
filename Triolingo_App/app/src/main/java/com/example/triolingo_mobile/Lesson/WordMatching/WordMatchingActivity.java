a22850d3227cd7248d324e965f72e3c802217b16
Minnminx <theultimatrix2@gmail.com>
2023-03-19 13:34:08 +0700

    Update word matching activity #2

--- Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Lesson/WordMatching/WordMatchingActivity.java
+++ Triolingo_App/app/src/main/java/com/example/triolingo_mobile/Lesson/WordMatching/WordMatchingActivity.java
@@ -2,12 +2,17 @@ package com.example.triolingo_mobile.Lesson.WordMatching;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.triolingo_mobile.DAO.LessonDAO;
 import com.example.triolingo_mobile.R;
+import com.example.triolingo_mobile.Util.LessonUtil;
+
+import java.util.HashMap;
 
 public class WordMatchingActivity extends AppCompatActivity {
+    private HashMap<String, String> Question_Answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,16 @@ public class WordMatchingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_word_matching_execise);
     }
 
-    void GetQuestionData(int lessonId) {
+    void GetQuestionData() {
 //        var LessonDAO
+        Intent intent = getIntent();
+        int exerciseID = intent.getIntExtra("exerciseId", -1);
+        if (exerciseID < 0) {
+            // next question
+        }
+    }
+
+    void finishExecercise() {
+
     }
 }
